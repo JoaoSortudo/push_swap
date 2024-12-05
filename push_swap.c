6bbc8f240d48c1f37c12d46d446ef880c93d06cb
jpires-n <luckyjoey13@gmail.com>
2024-12-05 15:10:10 -0300

    ajeitando a norma

--- push_swap.c
+++ push_swap.c
@@ -1,22 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpires-n <jpires-n@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-12-03 20:52:49 by jpires-n          #+#    #+#             */
+/*   Updated: 2024-12-03 20:52:49 by jpires-n         ###   ########.rio      */
+/*                                                                            */
+/* ************************************************************************** */
+
+
 #include "push_swap.h"
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     t_stack *stack_a;
     t_stack *stack_b;
 
-    if (argc < 2) {
+    if (argc < 2) 
+    {
         write(2, "Error\n", 6);
         return (1);
     }
 
     stack_a = init_stack();
     stack_b = init_stack();
-    if (!stack_a || !stack_b) {
+    if (!stack_a || !stack_b)
+    {
         write(2, "Error\n", 6);
         return (1);
     }
 
-    if (!fill_stack(stack_a, argc, argv)) {
+    if (!fill_stack(stack_a, argc, argv))
+    {
         write(2, "Error\n", 6);
         free_stack(stack_a);
         free_stack(stack_b);
--- push_swap.h
+++ push_swap.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpires-n <jpires-n@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-12-03 20:52:35 by jpires-n          #+#    #+#             */
+/*   Updated: 2024-12-03 20:52:35 by jpires-n         ###   ########.rio      */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
@@ -5,13 +17,13 @@
 # include <unistd.h>
 
 typedef struct s_node {
-    int             value;
-    struct s_node   *next;
-} t_node;
+	int				value;
+	struct s_node	*next;
+}	t_node;
 
 typedef struct s_stack {
-    t_node *top;
-    int    size;
+	t_node *top;
+	int    size;
 } t_stack;
 
 // Funções para as pilhas
